4d75893f67d20f3bcb1ef8f76cb42969c4e8e38a
nurulalll <nurulainilativa@gmail.com>
2023-12-02 17:07:06 +0700

    revisi responsif dan cekout

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Get the navbar element
+const navbar = document.querySelector("nav");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Function to add or remove the 'nav-scroll' class based on scroll position
+function handleScroll() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("nav-scroll");
+  } else {
+    navbar.classList.remove("nav-scroll");
+  }
+}
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
